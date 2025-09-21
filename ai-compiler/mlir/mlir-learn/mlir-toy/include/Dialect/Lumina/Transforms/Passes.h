70ae2f4a6f82217696ac24aa5ce966215f88d04f
up-to-star <2200316527@qq.com>
2025-09-21 23:57:25 +0800

    learn pass init

---
+++ ai-compiler/mlir/mlir-learn/mlir-toy/include/Dialect/Lumina/Transforms/Passes.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "mlir/Pass/Pass.h"
+
+namespace mlir::lumina {
+std::unique_ptr<mlir::Pass> createApplyDistributeTransformPass();
+
+#define GEN_PASS_DECL
+#define GEN_PASS_REGISTRATION
+#include "Dialect/Lumina/Transforms/Passes.h.inc"
+}  // namespace mlir::lumina
